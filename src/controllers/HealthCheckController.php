4fac1605e6b00031153475ad59623a36e6507018
Johannes Ahrndt <ahrndt@webhub.de>
2022-03-10 17:44:57 +0100

    Add secret to health check endpoint

--- src/controllers/HealthCheckController.php
+++ src/controllers/HealthCheckController.php
@@ -7,18 +7,37 @@ use craft\web\Controller;
 use OhDear\HealthCheckResults\CheckResults;
 use webhubworks\ohdear\health\checks\Check;
 use webhubworks\ohdear\OhDear;
+use yii\web\ForbiddenHttpException;
 
 class HealthCheckController extends Controller
 {
-
     protected $allowAnonymous = ['results'];
 
     public function actionResults()
     {
-        $checkResults = array_map(function(Check $check) {
-            return $check->run();
+        $this->ensureSecretIsValid();
+
+        $checkResults = array_map(function (Check $check) {
+            try {
+                return $check->run();
+            } catch (\Throwable $e) {
+                return $check->getGenericCrashResult($e->getMessage());
+            }
         }, OhDear::$plugin->health->registeredChecks());
 
         return (new CheckResults(null, $checkResults))->toJson();
+    }
+
+    private function ensureSecretIsValid()
+    {
+        $secretHeader = $this->request->headers->get('oh-dear-health-check-secret');
+
+        if (is_null($secretHeader)) {
+            throw new ForbiddenHttpException('Invalid secret');
+        }
+
+        if ($secretHeader !== OhDear::$plugin->settings->healthCheckSecret) {
+            throw new ForbiddenHttpException('Invalid secret');
+        }
     }
 }
