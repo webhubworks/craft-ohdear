0bef3008ba83b8d7025fe52d0bd1d9276ba16d5c
Johannes Ahrndt <ahrndt@webhub.de>
2023-10-05 11:27:20 +0200

    Improve error handling via API

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -1,9 +1,5 @@
 <?php
 /**
- * Oh Dear plugin for Craft CMS 4.x
- *
- * Integrate Oh Dear into Craft CMS.
- *
  * @link      https://webhub.de
  * @copyright Copyright (c) 2019 webhub GmbH
  */
@@ -11,11 +7,14 @@
 namespace webhubworks\ohdear\controllers;
 
 use Craft;
+use craft\helpers\App;
 use craft\web\Controller;
+use OhDear\PhpSdk\Exceptions\NotFoundException;
 use OhDear\PhpSdk\Exceptions\UnauthorizedException;
 use webhubworks\ohdear\OhDear;
 use yii\web\BadRequestHttpException;
 use yii\web\Response;
+use function PHPUnit\Framework\matches;
 
 class ApiController extends Controller
 {
@@ -26,35 +25,36 @@ class ApiController extends Controller
      * it will be taken instead of the token that
      * may be present in the plugin settings.
      *
-     * @see https://ohdear.app/swagger#/sites/get_sites
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionSites()
+    public function actionSites(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
 
         $apiTokenParam = Craft::$app->request->getQueryParam('api-token', null);
 
-        if ($apiTokenParam === null) {
-            $sites = OhDear::$plugin->api->getSites();
-        } else {
-            $sites = OhDear::$plugin->settingsService->getSites(
-                Craft::parseEnv($apiTokenParam)
-            );
-        }
+        try {
+            if ($apiTokenParam === null) {
+                $sites = OhDear::$plugin->api->getSites();
+            } else {
+                $sites = OhDear::$plugin->settingsService->getSites(
+                    App::parseEnv($apiTokenParam)
+                );
+            }
 
-        return $this->asJson([
-            'sites' => $sites,
-        ]);
+            return $this->asJson([
+                'sites' => $sites,
+            ]);
+        } catch (UnauthorizedException $e) {
+            return $this->handleInvalidApiTokenError();
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionSite()
+    public function actionSite(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -63,18 +63,15 @@ class ApiController extends Controller
             return $this->asJson([
                 'site' => OhDear::$plugin->api->getSite(),
             ]);
-        } catch (UnauthorizedException $e) {
-            return $this->asJson([
-                'error' => $e->getMessage(),
-            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
         }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionUptime()
+    public function actionUptime(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -84,16 +81,19 @@ class ApiController extends Controller
         $endedAt = $request->getRequiredQueryParam('endedAt');
         $split = $request->getQueryParam('split', null);
 
-        return $this->asJson([
-            'uptime' => OhDear::$plugin->api->getUptime($startedAt, $endedAt, $split),
-        ]);
+        try {
+            return $this->asJson([
+                'uptime' => OhDear::$plugin->api->getUptime($startedAt, $endedAt, $split),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionPaddedUptime()
+    public function actionPaddedUptime(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -103,18 +103,21 @@ class ApiController extends Controller
         $endedAt = $request->getRequiredQueryParam('endedAt');
         $split = $request->getQueryParam('split', null);
 
-        return $this->asJson([
-            'uptime' => OhDear::$plugin->api->leftPadUptimeToMonday(
-                OhDear::$plugin->api->getUptime($startedAt, $endedAt, $split)
-            ),
-        ]);
+        try {
+            return $this->asJson([
+                'uptime' => OhDear::$plugin->api->leftPadUptimeToMonday(
+                    OhDear::$plugin->api->getUptime($startedAt, $endedAt, $split)
+                ),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionDowntime()
+    public function actionDowntime(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -123,72 +126,87 @@ class ApiController extends Controller
         $startedAt = $request->getRequiredQueryParam('startedAt');
         $endedAt = $request->getRequiredQueryParam('endedAt');
 
-        return $this->asJson([
-            'downtime' => OhDear::$plugin->api->getDowntime($startedAt, $endedAt),
-        ]);
+        try {
+            return $this->asJson([
+                'downtime' => OhDear::$plugin->api->getDowntime($startedAt, $endedAt),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionBrokenLinks()
+    public function actionBrokenLinks(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
 
-        return $this->asJson([
-            'brokenLinks' => OhDear::$plugin->api->getBrokenLinks(),
-        ]);
+        try {
+            return $this->asJson([
+                'brokenLinks' => OhDear::$plugin->api->getBrokenLinks(),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionMixedContent()
+    public function actionMixedContent(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
 
-        return $this->asJson([
-            'mixedContentItems' => OhDear::$plugin->api->getMixedContent(),
-        ]);
+        try {
+            return $this->asJson([
+                'mixedContentItems' => OhDear::$plugin->api->getMixedContent(),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionCertificateHealth()
+    public function actionCertificateHealth(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
 
-        return $this->asJson([
-            'certificateHealth' => OhDear::$plugin->api->getCertificateHealth(),
-        ]);
+        try {
+            return $this->asJson([
+                'certificateHealth' => OhDear::$plugin->api->getCertificateHealth(),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionApplicationHealthChecks()
+    public function actionApplicationHealthChecks(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
 
-        return $this->asJson([
-            'applicationHealthChecks' => OhDear::$plugin->api->getApplicationHealthChecks(),
-        ]);
+        try {
+            return $this->asJson([
+                'applicationHealthChecks' => OhDear::$plugin->api->getApplicationHealthChecks(),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionApplicationHealthCheckResults()
+    public function actionApplicationHealthCheckResults(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -197,44 +215,53 @@ class ApiController extends Controller
             \Craft::$app->request->getRequiredQueryParam('checkId')
         );
 
-        return $this->asJson([
-            'applicationHealthCheckResults' => OhDear::$plugin->api->getApplicationHealthCheckResults($checkId),
-        ]);
+        try {
+            return $this->asJson([
+                'applicationHealthCheckResults' => OhDear::$plugin->api->getApplicationHealthCheckResults($checkId),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionCronChecks()
+    public function actionCronChecks(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
 
-        return $this->asJson([
-            'cronChecks' => OhDear::$plugin->api->getCronChecks(),
-        ]);
+        try {
+            return $this->asJson([
+                'cronChecks' => OhDear::$plugin->api->getCronChecks(),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionCurrentPerformance()
+    public function actionCurrentPerformance(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
 
-        return $this->asJson([
-            'currentPerformance' => OhDear::$plugin->api->getCurrentPerformance(),
-        ]);
+        try {
+            return $this->asJson([
+                'currentPerformance' => OhDear::$plugin->api->getCurrentPerformance(),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionPerformance()
+    public function actionPerformance(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -244,16 +271,19 @@ class ApiController extends Controller
         $end = $request->getRequiredQueryParam('end');
         $groupBy = $request->getQueryParam('groupBy');
 
-        return $this->asJson([
-            'performance' => OhDear::$plugin->api->getPerformance($start, $end, $groupBy),
-        ]);
+        try {
+            return $this->asJson([
+                'performance' => OhDear::$plugin->api->getPerformance($start, $end, $groupBy),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionDisableCheck()
+    public function actionDisableCheck(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -262,16 +292,19 @@ class ApiController extends Controller
             \Craft::$app->request->getRequiredBodyParam('checkId')
         );
 
-        return $this->asJson([
-            'check' => OhDear::$plugin->api->disableCheck($checkId),
-        ]);
+        try {
+            return $this->asJson([
+                'check' => OhDear::$plugin->api->disableCheck($checkId),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionEnableCheck()
+    public function actionEnableCheck(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -280,16 +313,19 @@ class ApiController extends Controller
             \Craft::$app->request->getRequiredBodyParam('checkId')
         );
 
-        return $this->asJson([
-            'check' => OhDear::$plugin->api->enableCheck($checkId),
-        ]);
+        try {
+            return $this->asJson([
+                'check' => OhDear::$plugin->api->enableCheck($checkId),
+            ]);
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
+        }
     }
 
     /**
-     * @return Response
      * @throws BadRequestHttpException
      */
-    public function actionRequestRun()
+    public function actionRequestRun(): Response
     {
         $this->requireAcceptsJson();
         $this->requireLogin();
@@ -302,9 +338,33 @@ class ApiController extends Controller
             return $this->asJson([
                 'check' => OhDear::$plugin->api->requestRun($checkId),
             ]);
-        } catch (\Exception $e) {
-            $error = json_decode($e->getMessage(), true);
-            return $this->asErrorJson($error['message'] ?? 'An error occurred');
+        } catch (UnauthorizedException|NotFoundException $e) {
+            return $this->handleError($e);
         }
+    }
+
+    private function handleError($e): Response
+    {
+        return match (get_class($e)) {
+            NotFoundException::class => $this->handleInvalidSiteIdError(),
+            UnauthorizedException::class => $this->handleInvalidApiTokenError(),
+        };
+
+    }
+
+    private function handleInvalidApiTokenError(): Response
+    {
+        $this->response->setStatusCode(403);
+        return $this->asJson([
+            'error' => 'The Oh Dear API token is invalid.',
+        ]);
+    }
+
+    private function handleInvalidSiteIdError(): Response
+    {
+        $this->response->setStatusCode(404);
+        return $this->asJson([
+            'error' => 'The Oh Dear site could not be found. Please check your site ID.',
+        ]);
     }
 }
