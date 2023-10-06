a044b5607c25ede06bd792c6a978e71392566d99
Johannes Ahrndt <johannes.ahrndt@rostock-ahoi.de>
2023-10-06 18:10:41 +0200

    Add delete action, fix bug with timezone mismatch

--- src/console/controllers/MaintenanceController.php
+++ src/console/controllers/MaintenanceController.php
@@ -11,6 +11,7 @@
 namespace webhubworks\ohdear\console\controllers;
 
 use craft\helpers\Console;
+use craft\helpers\DateTimeHelper;
 use Exception;
 use OhDear\PhpSdk\Resources\MaintenancePeriod;
 use webhubworks\ohdear\OhDear;
@@ -20,112 +21,136 @@ use yii\console\widgets\Table;
 
 class MaintenanceController extends Controller
 {
+    use HandlesConsoleErrors;
+
     /**
      * Retrieve a list of all maintenance periods for the site.
-     *
-     * @return int
      *
      * @author    webhub GmbH
      * @package   OhDear
      * @since     1.0.0
      */
-    public function actionList()
+    public function actionList(): int
     {
-        $periods = OhDear::$plugin->api->maintenancePeriods();
-
-        if (empty($periods)) {
-            $this->stdout("No maintenance periods for this site." . PHP_EOL, Console::FG_GREEN);
-            return ExitCode::OK;
-        }
-
-        usort($periods, function($a, $b) {
-            return strtotime($a->startsAt) > strtotime($b->startsAt);
-        });
-
         try {
-            $this->displayTable($periods);
+            $periods = OhDear::$plugin->api->maintenancePeriods();
+
+            if (empty($periods)) {
+                $this->stdout("No maintenance periods for this site." . PHP_EOL, Console::FG_GREEN);
+                return ExitCode::OK;
+            }
+
+            usort($periods, function ($a, $b) {
+                return strtotime($a->startsAt) > strtotime($b->startsAt);
+            });
+
+            try {
+                $this->displayTable($periods);
+            } catch (Exception $e) {
+                $this->stdout($e->getMessage() . PHP_EOL, Console::FG_RED);
+                return ExitCode::UNSPECIFIED_ERROR;
+            }
+
+            return ExitCode::OK;
         } catch (Exception $e) {
-            $this->stdout($e->getMessage() . PHP_EOL, Console::FG_RED);
+            $this->stdout($this->parseErrorMessage($e) . PHP_EOL, Console::FG_RED);
             return ExitCode::UNSPECIFIED_ERROR;
         }
-
-        return ExitCode::OK;
     }
 
     /**
      * Start a maintenance period for the site.
      *
      * @param int $stopAfter Stop the maintenance period after X amount of seconds (defaults to one hour)
-     * @return int
      *
-     * @author    webhub GmbH
+     * @throws \Throwable
      * @package   OhDear
      * @since     1.0.0
+     * @author    webhub GmbH
      */
-    public function actionStart(int $stopAfter = 60 * 60)
+    public function actionStart(int $stopAfter = 60 * 60): int
     {
-        $period = OhDear::$plugin->api->startMaintenancePeriod($stopAfter);
-
-        $this->stdout("Maintenance period started." . PHP_EOL, Console::FG_GREEN);
-
         try {
+            $period = OhDear::$plugin->api->startMaintenancePeriod($stopAfter);
+
+            $this->stdout("Maintenance period started." . PHP_EOL, Console::FG_GREEN);
+
             $this->displayTable([$period]);
+
+            return ExitCode::OK;
         } catch (Exception $e) {
-            $this->stdout($e->getMessage() . PHP_EOL, Console::FG_RED);
+            $this->stdout($this->parseErrorMessage($e) . PHP_EOL, Console::FG_RED);
             return ExitCode::UNSPECIFIED_ERROR;
         }
-
-        return ExitCode::OK;
     }
 
     /**
      * Stop the active maintenance period for the site.
      *
-     * @return int
+     * @author    webhub GmbH
+     * @package   OhDear
+     * @since     1.0.0
+     */
+    public function actionStop(): int
+    {
+        try {
+            OhDear::$plugin->api->stopMaintenancePeriod();
+
+            $this->stdout("Maintenance period stopped." . PHP_EOL, Console::FG_GREEN);
+
+            return ExitCode::OK;
+        } catch (Exception $e) {
+            $this->stdout($this->parseErrorMessage($e) . PHP_EOL, Console::FG_RED);
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
+    }
+
+    /**
+     * Stop the active maintenance period for the site.
      *
      * @author    webhub GmbH
      * @package   OhDear
      * @since     1.0.0
      */
-    public function actionStop()
+    public function actionDelete(int $id): int
     {
-        OhDear::$plugin->api->stopMaintenancePeriod();
+        try {
+            OhDear::$plugin->api->deleteMaintenancePeriod($id);
 
-        $this->stdout("Maintenance period stopped." . PHP_EOL, Console::FG_GREEN);
+            $this->stdout("Maintenance period deleted." . PHP_EOL, Console::FG_GREEN);
 
-        return ExitCode::OK;
+            return ExitCode::OK;
+        } catch (Exception $e) {
+            $this->stdout($this->parseErrorMessage($e) . PHP_EOL, Console::FG_RED);
+            return ExitCode::UNSPECIFIED_ERROR;
+        }
     }
 
     /**
-     * @param array $periods
-     * @throws Exception
+     * @throws \Throwable
      */
-    private function displayTable($periods)
+    private function displayTable(array $periods): void
     {
-        $rows = array_map(function($period) {
+        $rows = array_map(function ($period) {
             $startTime = strtotime($period->startsAt);
+            $startTime = DateTimeHelper::toDateTime($startTime);
             $endTime = strtotime($period->endsAt);
+            $endTime = DateTimeHelper::toDateTime($endTime);
 
-            if (time() > $endTime) {
-                $status = 'past';
-            } elseif (time() < $startTime) {
-                $status = 'scheduled';
-            } else {
-                $status = 'active';
-            }
 
             /** @var MaintenancePeriod $period */
             return [
                 $period->id,
                 $period->siteId,
-                $period->startsAt,
-                $period->endsAt,
-                $status,
+                $startTime->format('Y-m-d H:i'),
+                $endTime ? $endTime->format('Y-m-d H:i') : '∞',
             ];
         }, $periods);
 
+        $this->stdout('Note: Times are displayed in your Oh Dear team timezone.' . PHP_EOL, Console::FG_YELLOW);
+
         echo Table::widget([
-            'headers' => $headers = ['ID', 'Site ID', 'Starts at', 'Ends at', 'Status'],
+            'headers' => ['ID', 'Site ID', 'Starts at', 'Ends at'],
             'rows' => $rows,
         ]);
     }
