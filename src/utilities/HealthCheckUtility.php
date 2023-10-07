ac10fcdd17dcaf320079f565f2ed5001a19cfb46
Johannes Ahrndt <johannes.ahrndt@rostock-ahoi.de>
2023-10-07 16:07:01 +0200

    Add utility section about latest queue heartbeat

--- src/utilities/HealthCheckUtility.php
+++ src/utilities/HealthCheckUtility.php
@@ -3,6 +3,8 @@
 namespace webhubworks\ohdear\utilities;
 
 use craft\base\Utility;
+use craft\helpers\DateTimeHelper;
+use webhubworks\ohdear\health\checks\QueueCheck;
 use webhubworks\ohdear\OhDear;
 
 class HealthCheckUtility extends Utility
@@ -29,11 +31,18 @@ class HealthCheckUtility extends Utility
     public static function contentHtml(): string
     {
         // 😅
-        $results = json_encode(json_decode(OhDear::$plugin->health->getCheckResults()->toJson()), JSON_PRETTY_PRINT);
-        $heading = \Craft::t('ohdear', 'Current check results');
+        $queueHealthHeartbeat = \Craft::$app->getCache()->get(QueueCheck::CACHE_KEY);
+        $queueHealthHeartbeatString = $queueHealthHeartbeat === false ? 'Did not run yet. Did you schedule the queue health check command?' : $queueHealthHeartbeat;
+        $date = $queueHealthHeartbeat ? DateTimeHelper::toDateTime($queueHealthHeartbeat)->format('Y-m-d H:i:s e') : null;
+        $checkResults = json_encode(json_decode(OhDear::$plugin->health->getCheckResults()->toJson()), JSON_PRETTY_PRINT);
+        $heading1 = \Craft::t('ohdear', 'Latest heartbeat from Queue');
+        $heading2 = \Craft::t('ohdear', 'Current check results');
         return <<<HTML
-<h2>$heading</h2>
-<pre><code style="background-color: #282a35;color: #50FA7B;padding: .5rem;border-radius: .25rem">$results</code></pre>
+<h2>$heading1</h2>
+<p>Queue Health Check docs on <a href="https://github.com/webhubworks/craft-ohdear/wiki/Queue-Health-Check" target="_blank">Github</a>.</p>
+<pre><code style="background-color: #282a35;color: #50FA7B;padding: .5rem;border-radius: .25rem">$queueHealthHeartbeatString\n$date</code></pre>
+<h2>$heading2</h2>
+<pre><code style="background-color: #282a35;color: #50FA7B;padding: .5rem;border-radius: .25rem">$checkResults</code></pre>
 HTML;
     }
 }
